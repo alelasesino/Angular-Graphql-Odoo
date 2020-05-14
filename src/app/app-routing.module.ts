a362280ff9118eeb1a220c41909d1309f9c6a529
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-14 21:05:10 +0200

    Added menu route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MenuComponent } from './menu/components/menu/menu.component';
 
 
-const routes: Routes = [
-  {path: 'menu', pathMatch: 'full', component: MenuComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
