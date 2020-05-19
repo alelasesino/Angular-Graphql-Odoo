beb732c515ede71b6ca3c2e8e5d7ef4035f2ff00
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-19 22:09:57 +0200

    Implements Reception Query

--- src/app/inventory/components/reception/reception.component.ts
+++ src/app/inventory/components/reception/reception.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { OriginLocationComponent } from '../origin-location/origin-location.component';
 import { Router } from '@angular/router';
+import { InventoryService } from '../../services/inventory.service';
 
 @Component({
   selector: 'app-reception',
@@ -9,21 +10,34 @@ import { Router } from '@angular/router';
 })
 export class ReceptionComponent implements OnInit {
 
-  headers = {name: 'Nombre', time: 'Hora'};
-  data = [
-    {name: 'Mi nombre', time: '05:00'},
-    {name: 'Mi as', time: '06:00'},
-    {name: 'Mi ff', time: '07:00'},
-    {name: 'Mi aa', time: '08:00'},
-    {name: 'Mi fffb', time: '09:00'}
+  private headers: object = {code: 'Referencia', display_name: 'Articulo', time: 'Hora', quantity: 'Cantidad'};
+  private data = [
+    //{id: 3, code: '4 KG GRANEL', display_name: 'Caja de granel3', time: '18:00', quantity: '251'}
   ]
 
   @ViewChild('origin', {static: true}) origin: OriginLocationComponent;
 
-  constructor(private router: Router) { }
+  constructor(private service:InventoryService, private router: Router) { }
 
   ngOnInit() {
     //console.log(this.origin);
+
+    this.service.getReceptions().subscribe(result => {
+
+      result.data.receptions.forEach(reception => {
+
+        const time = reception.time;
+
+        reception.receive_products.forEach(product => {
+
+          this.data.push({id: product.id, code: product.code, display_name: product.display_name, time: time, quantity: product.kilos});
+
+        });
+
+      });
+
+    });
+
   }
 
   navigateToRegister(){
