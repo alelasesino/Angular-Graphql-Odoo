3e5173cc1b89f87afa447db0904c9f57857217a9
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-20 00:33:33 +0200

    Implements Register Reception Service

--- src/app/inventory/components/register-reception/register-reception.component.ts
+++ src/app/inventory/components/register-reception/register-reception.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'apollo-link';
 import { DataTableComponent } from '../data-table/data-table.component';
+import { RegisterReceptionService } from '../../services/register-reception.service';
 
 @Component({
   selector: 'app-register-reception',
@@ -15,14 +16,14 @@ export class RegisterReceptionComponent implements OnInit {
   private loading: boolean;
   private headers: object = {code: 'Referencia', display_name: 'Articulo', quantity: 'Cantidad'};
   private data = [
-    {id: 1, code: '4 KG GRANEL', display_name: 'Caja de granel1', quantity: '251'},
-    {id: 2, code: '4 KG GRANEL', display_name: 'Caja de granel2', quantity: '251'},
-    {id: 3, code: '4 KG GRANEL', display_name: 'Caja de granel3', quantity: '251'}
+    // {id: 1, code: '4 KG GRANEL', display_name: 'Caja de granel1', quantity: '251'},
+    // {id: 2, code: '4 KG GRANEL', display_name: 'Caja de granel2', quantity: '251'},
+    // {id: 3, code: '4 KG GRANEL', display_name: 'Caja de granel3', quantity: '251'}
   ]
 
   state: any;
 
-  constructor() { }
+  constructor(private service: RegisterReceptionService, private router: Router) { }
 
   ngOnInit() {
 
@@ -34,6 +35,37 @@ export class RegisterReceptionComponent implements OnInit {
     this.table.addItem(product);
   }
 
-  confirmReception(){}
+  confirmReception(){
+
+    const reception = {
+      farm_id: 31,
+      products: this.getProducts()
+    };
+
+    this.loading = true;
+
+    this.service.mutate({reception}).subscribe(result => {
+
+      this.loading = false;
+      console.log(result);
+      this.router.navigate(['/reception']);
+
+    });
+
+  }
+
+  private getProducts(){
+
+    const products = [];
+
+    this.data.forEach(element => {
+      
+      products.push({id: element.id, lot: "255", quantity: Number(element.quantity)});
+
+    });
+
+    return products;
+
+  }
 
 }
