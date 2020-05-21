ba2f66bf6906b0cdc0c1eae4a46956238aecd924
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-21 22:12:05 +0200

    Added AuthGuard All Paths

--- src/app/inventory/inventory-routing.module.ts
+++ src/app/inventory/inventory-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReceptionComponent } from './components/reception/reception.component';
 import { RegisterReceptionComponent } from './components/register-reception/register-reception.component';
+import { AuthGuard } from '../login/guards/auth.guard';
 
 
 const routes: Routes = [
-  {path: 'reception', pathMatch: 'full', component: ReceptionComponent},
-  {path: 'reception/register', pathMatch: 'full', component: RegisterReceptionComponent}
+  {path: 'reception', pathMatch: 'full', component: ReceptionComponent, canActivate: [AuthGuard]},
+  {path: 'reception/register', pathMatch: 'full', component: RegisterReceptionComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
