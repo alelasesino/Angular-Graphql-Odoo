becd04510ab5808cc32989e5adf55b34e8ef12b9
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-31 18:20:13 +0200

    Update Auth Service

--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import { ToastService } from 'src/app/inventory/services/toast.service';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { timeout, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toast: ToastService) { }
 
   login(username, password){
 
@@ -20,7 +22,16 @@ export class AuthService {
       }
     };
 
-    return this.http.post<any>(path, body);
+    return this.http.post<any>(path, body).pipe(
+      
+      timeout(5000), catchError(err => {
+
+        this.toast.showError("No se ha podido establecer conexión con el servidor", "Error Conexión");
+        throw new Error('Error Connection'); 
+
+      })
+
+    );
 
   }
 
