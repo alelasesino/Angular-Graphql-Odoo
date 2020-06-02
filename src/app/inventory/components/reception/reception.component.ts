63ac48f0fe9cff458560bcf2ccf737ba28539bf9
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-06-03 00:34:54 +0200

    Update Reception Component

--- src/app/inventory/components/reception/reception.component.ts
+++ src/app/inventory/components/reception/reception.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { OriginLocationComponent } from '../origin-location/origin-location.component';
 import { Router } from '@angular/router';
 import { InventoryService } from '../../services/inventory.service';
@@ -10,11 +10,18 @@ import { InventoryService } from '../../services/inventory.service';
 })
 export class ReceptionComponent {
 
-  headers: object = {code: 'Referencia', display_name: 'Articulo', time: 'Hora', quantity: 'Cantidad'};
-  private lot: string = "LOTE: L01052403";
-  data = [
-    //{id: 3, code: '4 KG GRANEL', display_name: 'Caja de granel3', time: '18:00', quantity: '251'}
+  rows = [
+    // {id: 3, code: '4 KG GRANEL', displayName: 'Caja de granel3', time: '12:00', quantity: '251'},
+    // {id: 3, code: '4 KG GRANEL', displayName: 'Caja de granel3', time: '17:00', quantity: '251'},
+    // {id: 3, code: '4 KG GRANEL', displayName: 'Caja de granel3', time: '19:00', quantity: '251'}
   ]
+  
+  messages = {
+    emptyMessage: "¡No se ha registrado ninguna recepción hoy!"
+  }
+  
+  lot: string;
+  totalQuantity: number = 0;
 
   constructor(private service:InventoryService, private router: Router) { }
 
@@ -23,25 +30,35 @@ export class ReceptionComponent {
     this.lot = lot
 
     this.service.getReceptions(lot).subscribe(result => {
-
+      
       result.data.receptions.forEach(reception => {
 
         const time = reception.time;
 
         reception.receiveProducts.forEach(product => {
-
-          this.data.push({id: product.id, code: product.code, display_name: product.displayName, time: time, quantity: product.kilos});
+          
+          this.rows.push({id: product.id, code: product.code, displayName: product.displayName, time: time, quantity: product.kilos});
 
         });
 
       });
+
+      this.rows = [...this.rows];
+      this.calculateTotalQuantity();
 
     });
 
   }
 
   navigateToRegister() {
-    this.router.navigateByUrl('reception/register', {state: {farm: "Finca", parcel: "Parcela"}});
+    this.router.navigateByUrl('reception/register');
+  }
+
+  calculateTotalQuantity() {
+    this.totalQuantity = 0;
+    this.rows.forEach(row => {
+      this.totalQuantity += Number(row.quantity);
+    });
   }
 
 }
