b57aab80da588c423b82af6671d4b7b7a5d7955f
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-06-03 00:51:15 +0200

    Update Inventory Service

--- src/app/inventory/services/inventory.service.ts
+++ src/app/inventory/services/inventory.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Apollo, QueryRef } from 'apollo-angular';
-import { timeout, catchError } from 'rxjs/operators';
-import { ToastService } from 'src/app/inventory/services/toast.service';
 import gql from 'graphql-tag'
 
 @Injectable({
@@ -12,7 +10,7 @@ export class InventoryService {
   private query: QueryRef<any>;
   private TIME_OUT = 5000;
 
-  constructor(private apollo: Apollo, private toast: ToastService) { }
+  constructor(private apollo: Apollo) { }
   
   public getFarmsAndProducts(){
     
@@ -134,16 +132,7 @@ export class InventoryService {
 
     this.query = this.apollo.watchQuery({query: RECEPTION_QUERY, variables: {today: false, lot}});
 
-    return this.query.valueChanges.pipe(
-
-      timeout(this.TIME_OUT), catchError(err => {
-
-        this.showConnectionError();
-        throw new Error('Error Connection'); 
-
-      })
-
-    );
+    return this.query.valueChanges;
 
   }
 
@@ -151,22 +140,8 @@ export class InventoryService {
 
     this.query = this.apollo.watchQuery({query: query});
 
-    return this.query.valueChanges.pipe(
+    return this.query.valueChanges;
 
-      timeout(this.TIME_OUT), catchError(err => {
-
-        this.showConnectionError();
-        console.log(err);
-        throw new Error('Error Connection'); 
-
-      })
-
-    );
-
-  }
-
-  private showConnectionError() {
-    this.toast.showError("No se ha podido establecer conexión con el servidor", "Error Conexión");
   }
 
 }
